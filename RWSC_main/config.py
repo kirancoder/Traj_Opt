ec5b0a6de17e55470f09c6ce196771466c0df27e
Kiran M A <kiranaralikatti6000@gmail.com>
2026-05-25 23:59:36 +0530

    changed folder name

--- RWSC_main/config.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# initial conditions    
-x_0 = 5  # x(0) = 5
-y_0 = 2  # y(0) = 2
-u_0 = 2  # u(0) = 2
-v_0 = 4  # v(0) = 4
-theta_0 = np.pi/40  # theta(0) = pi/40
-m_0 = 10000  # m(0) = 10000
